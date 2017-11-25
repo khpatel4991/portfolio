88b56e28ffe01cf86eff754f3001f20eedc2d931
Kashyap Patel <khpatel4991@gmail.com>
2017-11-24 19:00:03 -0500

    Added material ui

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,16 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Button from 'material-ui/Button';
+import Typography from 'material-ui/Typography';
+import { withStyles } from 'material-ui/styles';
+import withRoot from '../src/hoc/WithRoot';
+
+const styles = {
+  root: {
+    textAlign: 'center',
+    paddingTop: 200,
+  },
+};
 
 class Home extends Component {
   componentDidMount() {
@@ -7,12 +19,25 @@ class Home extends Component {
 
   render() {
     return (
-      <div>
-        Hi, this is Kashyap Patel.
-        This is my Portfolio Site.
+      <div className={this.props.classes.root}>
+        <Typography type="display1" gutterBottom>
+          Kashyap Patel
+        </Typography>
+        <Typography type="subheading" gutterBottom>
+          Portfolio Site
+        </Typography>
+        <Button raised color="accent">
+          Contact Me
+        </Button>
       </div>
     );
   }
 }
 
-export default Home;
+Home.propTypes = {
+  classes: PropTypes.shape({
+    root: PropTypes.string.isRequired,
+  }).isRequired,
+};
+
+export default withRoot(withStyles(styles)(Home));
